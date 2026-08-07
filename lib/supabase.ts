d5b33ab8c1bb1faf89e9d2f47b611df8221ef39b
Yogeendra <yogeendra.nsm9@gmail.com>
2026-08-07 11:18:06 +0530

    Export createClient function in lib/supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,8 +1,7 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient as createSupabaseClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string;
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
-  realtime: { params: { eventsPerSecond: 20 } },
-});
+export const createClient = () => {
+  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+  const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+  return createSupabaseClient(supabaseUrl, supabaseAnonKey);
+};
