5e99eae9deb125f7bbcbe6884ccda355700ed6f3
Yogeendra <yogeendra.nsm9@gmail.com>
2026-08-07 11:51:25 +0530

    Fix URL protocol format in lib/supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,8 +1,11 @@
 import { createClient as createSupabaseClient } from '@supabase/supabase-js';
 
 export const createClient = () => {
-  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-  const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+  const supabaseUrl =
+    process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://taeseyhaxvoosbiecxjx.supabase.co';
+  const supabaseAnonKey =
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'sb_publishable_qaBzjTnUelzuX1zlYd25EQ_VpjM_db0';
+
   return createSupabaseClient(supabaseUrl, supabaseAnonKey);
 };
 
